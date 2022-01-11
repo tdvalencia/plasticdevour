96ff5e77ad8fcf43831aa8b0fe70b97113d91994
Tony <tdv1130@gmail.com>
2022-01-10 22:00:24 -0800

    Start the RPI

---
+++ workspace/trshbt/src/main.cpp
@@ -0,0 +1,10 @@
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <inference_engine.hpp>
+#include <wiringPi.h>
+#include <iostream>
+
+int main() {
+	std::cout << "it's trashboat time!!" << std::endl;	
+}
